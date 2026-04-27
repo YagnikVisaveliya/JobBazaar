f92f2eb5686b6d0562f1d1f3a6c7a9a5d25e1758
yagnik-1125 <yagnikpatel6788@gmail.com>
2026-04-27 12:44:04 +0530

    feat: remove API_BASE_URL dependency from constants

--- FRONTEND/src/utils/constant.js
+++ FRONTEND/src/utils/constant.js
@@ -1,6 +1,6 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v2'
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v2'
 
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL || `${API_BASE_URL}/user`
-export const JOBS_URL = import.meta.env.VITE_JOBS_URL || `${API_BASE_URL}/jobs`
-export const APPLICATION_URL = import.meta.env.VITE_APPLICATION_URL || `${API_BASE_URL}/applications`
-export const COMPANY_URL = import.meta.env.VITE_COMPANY_URL || `${API_BASE_URL}/company`
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL 
+export const JOBS_URL = import.meta.env.VITE_JOBS_URL 
+export const APPLICATION_URL = import.meta.env.VITE_APPLICATION_URL 
+export const COMPANY_URL = import.meta.env.VITE_COMPANY_URL 
